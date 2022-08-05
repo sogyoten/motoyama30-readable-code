0dd283ef05f6c0b8d5418f6ee4ecaf685c5582c0
grape0606@gmail.com <grape0606@gmail.com>
2022-08-05 15:01:08 +0900

    spec8 : 仕様8まで実装

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,8 @@
 import sys
 
-def hasArgument(arg_list):
+def hasWordID(arg_list):
     """
-    引数が存在するかを調べる
+    単語IDが存在するかを調べる
 
     Parameters
     ----------
@@ -12,21 +12,22 @@ def hasArgument(arg_list):
     Returns
     -------
     bool
-        引数が存在するときTure
+        単語IDが存在するときTure
     """
-    if len(arg_list) >= 3:
+    if len(arg_list) >= 4:
         return True
     else :
         return False
 
 # definition and input        
-arg_list  = sys.argv #コマンドライン引数のリスト
+arg_list  = sys.argv #コマンドライン引数のリスト [user_name  data_file   *ID(任意)]
 word_dict = {}
 id = 1
 
 # file input to word_dict
 # dictionary_data.txtからデータを1行ずつ取得
-file_name = arg_list[1]
+user_name = arg_list[1]
+file_name = arg_list[2]
 with open(file_name) as file:
     for data in file:
         word_dict[id] = data.rstrip("\n")
@@ -34,8 +35,9 @@ with open(file_name) as file:
 file.close()
 
 # output
-if hasArgument(arg_list):
-    arg = int(arg_list[2]) #指定されたID
+print("ユーザー名: " + user_name)
+if hasWordID(arg_list):
+    arg = int(arg_list[3]) #指定されたID
     print(str(arg) + ": " + word_dict[arg])
 else :
     for id in word_dict:
