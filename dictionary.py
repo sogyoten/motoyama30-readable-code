ae79bc13bd387d2f3c3df4e8c7177eee224444d7
grape0606@gmail.com <grape0606@gmail.com>
2022-08-05 14:03:36 +0900

    spec5 : 仕様5を実装

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,12 @@
+word_dict={}
+id = 1
+
 # data.txtからデータを1行ずつ取得して出力
-with open("data.txt") as file:
+with open("dictionary_data.txt") as file:
     for data in file:
-        print(data.rstrip("\n"))
+        word_dict[id] = data.rstrip("\n")
+        id += 1
 file.close()
+
+for id in word_dict:
+    print(str(id) + ": " + word_dict[id])
